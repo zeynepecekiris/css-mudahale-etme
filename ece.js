8b9e7b67645c1b2a5ab2e801141a465a06bac699
zeynepecekiris <zeynep.ece933@gmail.com>
2024-07-09 21:57:40 +0300

    first commit

---
+++ ece.js
@@ -0,0 +1,8 @@
+const baslik=document.querySelector("h1");
+//baslik.setAttribute("style","padding:50px");
+
+console.log(baslik.style);
+
+baslik.style.padding="50px";
+baslik.style.fontSize="60px";
+baslik.style.fontWeight="bold";
